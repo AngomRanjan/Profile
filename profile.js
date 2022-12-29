fa5b8bd466932a4f36cea7f24c06f11b7908d263
AngomRanjan <angomranjan@gmail.com>
2022-12-30 02:51:53 +0530

    Remove Modal Popup

--- profile.js
+++ profile.js
@@ -101,32 +101,21 @@ const arrSocialMedia = [
   },
 ];
 // ref
-const projSnapshoot = (pImage, addClass = '') => {
+const projSnapshoot = (pImage) => {
   const snapshoot = document.createElement('div');
   snapshoot.className = 'snapshoot-placeholders';
   const image = document.createElement('img');
-  image.className = (addClass === '' ? 'snapshoot' : 'mc-snapshoot');
+  image.className = 'snapshoot';
   image.src = pImage;
   snapshoot.appendChild(image);
   return snapshoot;
 };
 
-const projTitle = (pName, addClass = 'project-titles my-15') => {
+const projTitle = (pName) => {
   const title = document.createElement('h2');
-  title.classList = addClass;
-  if (addClass === 'mc-title') title.id = 'mc-title';
+  title.classList = 'project-titles my-15';
   title.textContent = pName;
   return title;
-};
-
-const mcTbar = (pName) => {
-  const titleBar = document.createElement('div');
-  [titleBar.classList, titleBar.id] = ['mc-titlebar', 'mc-titlebar'];
-  titleBar.appendChild(projTitle(pName, 'mc-title'));
-  const mcBtn = document.createElement('button');
-  [mcBtn.type, mcBtn.className, mcBtn.id, mcBtn.textContent] = ['button', 'mc-btn', 'mc-close', '\u2715'];
-  titleBar.appendChild(mcBtn);
-  return titleBar;
 };
 
 const projSummary = (summaries) => {
@@ -142,20 +131,19 @@ const projSummary = (summaries) => {
   return ulSummaries;
 };
 
-const pDetails = (details, pid = null) => {
+const pDetails = (details) => {
   const pDescription = document.createElement('p');
   pDescription.className = 'project-descriptions';
-  if (pid !== null) pDescription.id = pid;
   pDescription.textContent = details;
   return pDescription;
 };
 
-const pSkills = (skills, adClass = 'tag') => {
+const pSkills = (skills) => {
   const ulSkills = document.createElement('ul');
   ulSkills.className = 'tags';
   const liSkills = skills.map((skill) => {
     const liSkill = document.createElement('li');
-    [liSkill.classList, liSkill.textContent] = [adClass, skill];
+    [liSkill.classList, liSkill.textContent] = ['tag', skill];
     return liSkill;
   });
   liSkills.forEach((skill) => ulSkills.appendChild(skill));
@@ -168,14 +156,14 @@ const cardBtn = (cid) => {
   return btn;
 };
 
-const modalBtnArea = (pLinks) => {
+const cardBtnCtn = (pLinks) => {
   const divBtn = document.createElement('div');
   divBtn.className = 'modal-btn-area';
   const mlink = pLinks.map((link, index) => {
     const imgIcon = document.createElement('img');
     [imgIcon.src, imgIcon.className] = ['icons/Icon.png', 'btn-icn'];
     const aLink = document.createElement('a');
-    [aLink.classList, aLink.href, aLink.id] = ['btn btn-modal', link, `mc-btn${index + 1}`];
+    [aLink.classList, aLink.href, aLink.target, aLink.id] = ['btn btn-modal', link, '_blank', `mc-btn${index + 1}`];
     aLink.textContent = (index === 0 ? 'See Live' : 'See Source');
     aLink.appendChild(imgIcon);
     return aLink;
@@ -194,8 +182,8 @@ function compileCards(project) {
   card.appendChild(projSnapshoot(`images/SnapshootD${project.no}.jpg`));
   const divPro = document.createElement('div');
   divPro.className = 'projects';
-  const arrFunc = [projTitle, projSummary, pDetails, pSkills, cardBtn];
-  const arr = [project.name, project.summaries, project.details, project.skills, project.no];
+  const arrFunc = [projTitle, projSummary, pDetails, pSkills, cardBtnCtn];
+  const arr = [project.name, project.summaries, project.details, project.skills, project.links];
   arrFunc.forEach((func, index) => divPro.appendChild(func(arr[index])));
   card.appendChild(divPro);
   return card;
@@ -212,34 +200,6 @@ for (let i = 0; i < arrProjects.length; i += 1) {
 const about = document.getElementById('about');
 about.parentNode.insertBefore(works, about);
 
-function compileModalCards(project) {
-  const modalCard = document.createElement('div');
-  modalCard.className = 'modal-card grid';
-  modalCard.id = 'modal-card';
-  const arrFunc = [mcTbar, projSummary, projSnapshoot, pDetails, pSkills, modalBtnArea];
-  const arr = [[project.name], [project.summaries], [`images/SnapshootD${project.no}.jpg`, 'mc-snapshoot'], [project.details, 'mc-desc'], [project.skills, 'tag mc-tags'], [project.links]];
-  arrFunc.forEach((func, index) => modalCard.appendChild(func(...arr[index])));
-  return modalCard;
-}
-
-function hideModal() {
-  const modPopUp = document.getElementById('modal-ui-wrapper');
-  modPopUp.classList.toggle('md-show');
-  document.body.classList.toggle('no-scroll');
-  document.body.removeChild(document.body.lastChild);
-}
-
-function showProjectDetails(proID) {
-  // showing  Project Info
-  const modalPopUp = document.createElement('div');//
-  modalPopUp.className = 'modal-ui';
-  modalPopUp.id = 'modal-ui-wrapper';
-  modalPopUp.appendChild(compileModalCards(arrProjects[proID - 1]));
-  document.body.appendChild(modalPopUp);
-  document.getElementById('mc-close').addEventListener('click', hideModal);
-  document.body.classList.toggle('no-scroll');
-}
-
 function compileSocial(social, index) {
   social.innerHTML = `<h3 class="head3 my-15" id="social-${index}">LET’S CONNECT</h3>
   <ul class='inline-li'>
@@ -249,14 +209,3 @@ function compileSocial(social, index) {
 
 const socialMedia = Array.from(document.getElementsByClassName('social-media'));
 socialMedia.forEach((social, i) => compileSocial(social, i + 1));
-
-// | Add Click Event Listners to Project Buttons
-const btns = Array.from(document.getElementsByClassName('btn-projects'));
-btns.forEach((btn) => {
-  btn.addEventListener('click', (event) => {
-    showProjectDetails(btn.id);
-    event.stopPropagation();
-  });
-});
-
-// || =========== Pop-UP Ends ==============|| */
